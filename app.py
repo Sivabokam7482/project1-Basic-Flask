082f022cb63215f17c3283da59af63664e01c41f
Sivabokam7482 <121338172+Sivabokam7482@users.noreply.github.com>
2023-02-28 17:02:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+#Create Application Instance
+# Application instance(AppIns) is an Object of Flask Class
+# So Import Flask Class
+# Flask class will take one arguement that is Name of current Module
+# standard way to represent current module name is __name__.
+
 from flask import Flask
 
 AppIns=Flask(__name__)
 
+# create one view function and decorate it with route method of AppIns
 
-@AppIns.route('/main')
-def main():
+@AppIns.route('/siva')
+def siva():
     return 'This is Flask'
 
 
